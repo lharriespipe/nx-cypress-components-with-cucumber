60c20f15aa8e9cae7749c855a75820f1b5419058
liam harries <lharries@ipipeline.com>
2023-07-05 12:39:21 +0100

    Added cypress config

---
+++ libs/test-lib/src/lib/component-test/component-test.component.cy.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { ComponentTestComponent } from './component-test.component';
+
+describe(ComponentTestComponent.name, () => {
+  beforeEach(() => {
+    TestBed.overrideComponent(ComponentTestComponent, {
+      add: {
+        imports: [],
+        providers: [],
+      },
+    });
+  });
+
+  it('renders', () => {
+    cy.mount(ComponentTestComponent);
+  });
+});
